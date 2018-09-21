d59dc8363859f42a62ce936b13d7b638dbad849c
osamtimizer <eaten.jb@gmail.com>
2018-09-21 10:37:02 +0900

    chapter9 finished

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   #of course you can write the hash with {}.
   #e.g. validates(:name, { "presence" => true })
 
+  #accessors
+  attr_accessor :remember_token
+
   #callbacks
   before_save { self.email = email.downcase }
 
@@ -15,8 +18,30 @@ class User < ApplicationRecord
   has_secure_password
 
   #methods
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
+
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  #class methods
+  class << self
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
 end
